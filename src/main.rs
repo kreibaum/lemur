f409ed57c14cb0c897a11d7da66a7c4ee8d3f441
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2024-06-22 00:36:42 +0200

    Page to add new cards to the deck

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,67 @@
-use crate::db::{create_card, establish_connection};
+use std::sync::Arc;
+
+use axum::{Form, response::Html, Router, routing::get};
+use axum::extract::State;
+use serde::Deserialize;
+use tera::Tera;
 
 mod db;
 mod models;
 mod schema;
 
-fn main() {
-    let mut connection = establish_connection();
-    let card = create_card(&mut connection, "Tokyo".to_string(), 35.6895, 139.6917).expect("Error creating card");
-    let cards = db::get_all_cards(&mut connection).expect("Error getting cards");
-    for card in cards {
-        println!("{:?}", card);
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // Initialize Tera
+    let tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    let tera = Arc::new(tera);
+
+    // Build our application with a route
+    let app = Router::new()
+        .route("/new_card", get(new_card_form).post(create_new_card))
+        .with_state(tera);
+
+    // Run our application
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn new_card_form(State(tera): State<Arc<Tera>>) -> Html<String> {
+    let context = tera::Context::new();
+    let rendered = tera.render("new_card.html", &context).unwrap();
+    Html(rendered)
+}
+
+
+#[derive(Deserialize)]
+struct NewCard {
+    place_name: String,
+    latitude: f32,
+    longitude: f32,
+}
+
+async fn create_new_card(
+    State(tera): State<Arc<Tera>>,
+    Form(new_card): Form<NewCard>,
+) -> Html<String> {
+    let mut conn = db::establish_connection();
+    match db::create_card(&mut conn, new_card.place_name, new_card.latitude, new_card.longitude) {
+        Ok(_) => {
+            let mut context = tera::Context::new();
+            context.insert("message", "Card created successfully!");
+            let rendered = tera.render("new_card.html", &context).unwrap();
+            Html(rendered)
+        }
+        Err(e) => {
+            let mut context = tera::Context::new();
+            context.insert("error", &format!("Error creating card: {}", e));
+            let rendered = tera.render("new_card.html", &context).unwrap();
+            Html(rendered)
+        }
     }
-    db::delete_card(&mut connection, card.id).expect("Error deleting card");
 }
